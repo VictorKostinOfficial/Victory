0fa98a79383dcb1df9c4e732c4ed5c6ff17d720b
Victor Kostin <victor.kostin.s@gmail.com>
2023-11-11 03:19:55 +0500

    Clean Repo

--- Victory/src/Victory.h
+++
@@ -1,7 +0,0 @@
-#include "Application.h"
-#include "EntryPoint.h"
-#include "Layer.h"
-
-#include "renderer/GraphicsRenderer.h"
-#include "renderer/vulkan_renderer/VulkanRenderer.h"
-#include "imgui.h"
